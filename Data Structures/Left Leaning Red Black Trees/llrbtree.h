4273583ebd14dc966d8e24461b1d7d39a4f79e80
AyishatOguntade <80053100+AyishatOguntade@users.noreply.github.com>
2025-03-13 12:07:03 -0400

    Update llrbtree.h

--- Data Structures/Left Leaning Red Black Trees/llrbtree.h
+++ Data Structures/Left Leaning Red Black Trees/llrbtree.h
@@ -19,3 +19,75 @@ create a LLRBT tree class
 
 
 */
+#include <iostream>
+#include <ostream>
+
+// Defines MAX
+// When used, MAX(a,b) will be replaced with ((a > b) ? a : b)
+#ifndef MAX
+#define MAX(a,b) ((a > b) ? a : b)
+#endif
+
+
+
+class RBTNode {
+private:
+    int data;
+    RBTNode* left;
+    RBTNode* right;
+    bool red;
+
+    void flipColors(){ 
+        red = !red; 
+        left->red = !left->red;
+        right->red = !right->red;
+    }
+
+public:
+    RBTNode();
+    RBTNode(int data);
+    ~RBTNode();
+
+    friend class RBTree;
+};
+
+class RBTree {
+private:
+    RBTNode* root;
+
+    void preorder(RBTNode* root, std::ostream& os);
+    void inorder(RBTNode* root, std::ostream& os);
+    void postorder(RBTNode* root, std::ostream& os);
+    RBTNode* rotateLeft(RBTNode* root);
+    RBTNode* rotateRight(RBTNode* root);
+    void destroy(RBTNode* root);
+    bool search(int data, RBTNode* root);
+    bool isred(RBTNode* root);
+    int height(RBTNode* root);
+    RBTNode* insert(int data, RBTNode* root);
+
+    // new remove functions 
+    RBTNode* moveRedLeft(RBTNode* h);
+    RBTNode* moveRedRight(RBTNode* h);
+    RBTNode* fixUp(RBTNode* h);
+    RBTNode* deleteMin(RBTNode* h);
+    RBTNode* deleteNode(RBTNode* h, int data);
+
+public:
+    RBTree();
+    ~RBTree();
+    void insert(int data);
+    void clear();
+    bool search(int data);
+    int height();
+    void remove(int data);
+    void deleteMin();
+    void preorder(std::ostream& os = std::cout);
+    void inorder(std::ostream& os = std::cout);
+    void postorder(std::ostream& os = std::cout);
+};
+
+
+
+
+
