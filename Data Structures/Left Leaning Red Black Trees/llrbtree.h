bdf0f14c02a46bd59607d8808ffe8321b5f7422e
AyishatOguntade <80053100+AyishatOguntade@users.noreply.github.com>
2025-03-13 12:02:16 -0400

    Add files via upload

---
+++ Data Structures/Left Leaning Red Black Trees/llrbtree.h
@@ -0,0 +1,21 @@
+
+/*
+create a node class
+    ptr to the left 
+    ptr to the right 
+    bool for color 
+    Note: what number will reperenset the black or red 
+    base constuctor 
+    constructor with a parameter 
+
+create a LLRBT tree class
+    a node that points to the root 
+    constuctor to set the root to null 
+    insert function 
+    clear function 
+    pre/post/inorder functions that output color 
+    flip color function 
+    height 
+
+
+*/
